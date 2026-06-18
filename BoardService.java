f1d932c228134c601711411a189513f2ee338356
C4t4ddict <visionsky10@gmail.com>
2026-06-18 15:17:07 +0900

    feat: BoardService 삭제 및 수정 기능 추가

--- BoardService.java
+++ BoardService.java
@@ -24,3 +24,19 @@ public class BoardService {
         board.setContent(newContent);
         return board;
     }
+    // feature/B: 삭제 기능 추가
+    public boolean delete(int id) {
+        Board board = findById(id);
+        if (board == null)
+            throw new IllegalArgumentException("게시글을 찾을 수 없습니다. ID: " + id);
+        return boards.remove(board);
+    }
+    // 수정 기능 추가
+    public Board update(int id, String newTitle, String newContent) {
+        Board board = findById(id);
+        if (board == null)
+            throw new IllegalArgumentException("게시글을 찾을 수 없습니다. ID: " + id);
+        board.setTitle(newTitle);
+        board.setContent(newContent);
+        return board;
+    }
